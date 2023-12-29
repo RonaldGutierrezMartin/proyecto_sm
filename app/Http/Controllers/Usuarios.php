cf70c315d95f1199ffc380f3e4cf3bbe16255b4d
AntonioJPL <ajpl1903@gmail.com>
2023-12-29 17:38:15 +0000

    creacion modelo usuario y controlador con primer metodo

---
+++ app/Http/Controllers/Usuarios.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Usuarios extends Controller
+{
+    function pintarRegistro(){
+        $tipos = DB::table("tiposusuario")->select("id", "nombre")->get();
+        return view("signUp", ["tipos" => $tipos]);
+    }
+}
