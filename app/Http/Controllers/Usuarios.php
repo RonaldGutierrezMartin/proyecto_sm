82bf180b7a5d9d7562507ad0577f800a95bdc495
Ronald <ronaldgutierrezmartin02@gmail.com>
2023-12-29 17:57:14 +0000

    creacion vistas

--- app/Http/Controllers/Usuarios.php
+++ app/Http/Controllers/Usuarios.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class Usuarios extends Controller
 {
-    function drawSingup(){
+    function drawSignUp(){
         $tipos = DB::table("tiposusuario")->select("id", "nombre")->get();
         return view("signUp", ["tipos" => $tipos]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Usuarios;
+use App\Models\Usuario;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get("/logIn", function(){
+    return view("logIn");
+});
+
+Route::get('/signUp', [Usuarios::class ,"drawSignUp"])->name("signUp");
+
