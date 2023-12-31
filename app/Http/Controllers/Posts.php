b627056efd111d7961497f1acc7c938082e0cb58
AntonioJPL <ajpl1903@gmail.com>
2023-12-31 18:44:08 +0000

    api con filtros

--- app/Http/Controllers/Posts.php
+++ app/Http/Controllers/Posts.php
@@ -6,11 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostCollection;
 use App\Models\Post;
+use App\Filters\PostFilter;
 
 class Posts extends Controller
 {
-    function index(){
-        $posts = Post::paginate();
-        return new PostCollection($posts);
+    function index(Request $request){
+        $filter = new PostFilter();
+        $queryItems = $filter->transform($request);
+        $posts = Post::where($queryItems);
+        return new PostCollection($posts->paginate()->appends($request->query()));
     }
 }
